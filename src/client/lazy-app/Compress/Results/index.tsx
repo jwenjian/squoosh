bb80468f46893cf4a7fa8a63e2e67aa3ebd40b39
Jiang Wenjian <jwenjian.me@gmail.com>
2025-02-19 14:31:14 +0800

    typescript sucks......

--- src/client/lazy-app/Compress/Results/index.tsx
+++ src/client/lazy-app/Compress/Results/index.tsx
@@ -61,7 +61,8 @@ export default class Results extends Component<Props, State> {
 
   private onCopy = () => {
     // copy a image file into clipboard
-    navigator.clipboard.write([new ClipboardItem({[_resultImageFile.type]: _resultImageFile as Blob})])
+    console.log(_resultImageFile)
+    navigator.clipboard.write([new ClipboardItem({['text/plain']: 'ok'})])
   }
 
   render(
