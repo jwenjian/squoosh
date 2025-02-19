15feb2a599a8cda7495f5a0173f0998a4b588930
Jiang Wenjian <jwenjian.me@gmail.com>
2025-02-19 14:04:25 +0800

    try to add copy icon and copy function

--- src/client/lazy-app/icons/index.tsx
+++ src/client/lazy-app/icons/index.tsx
@@ -92,6 +92,13 @@ export const DownloadIcon = () => (
   </svg>
 );
 
+export const CopyIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+  <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
+  <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+</svg>
+);
+
 export const SwapIcon = () => (
   <svg viewBox="0 0 18 14">
     <path d="M5.5 3.6v6.8L2.1 7l3.4-3.4M7 0L0 7l7 7V0zm4 0v14l7-7-7-7z" />
